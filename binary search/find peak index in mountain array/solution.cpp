f6ec96082d483c374f3542d4cebf088819df8eaf
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-10 11:49:00 +0530

    peak index in mountain array

---
+++ binary search/find peak index in mountain array/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& arr) {
+    int l=0, h=arr.size(), mid;
+    while(l<h)
+    {
+        mid = l+(h-l)/2;
+        if(arr[mid] < arr[mid+1])
+            l = mid+1;
+        else
+            h = mid;
+    }
+    return l;
+    }
+};
