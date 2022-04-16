5eac517e2e690560b42b3ccaf5bd9481aee75460
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-16 22:53:58 +0530

    solution using recursion

---
+++ stack/push an element to the bottom of a stack/solution Using Recursion.cpp
@@ -0,0 +1,40 @@
+stack<int> recur(stack<int> S, int N)
+{
+    // If stack is empty
+    if (S.empty())
+        S.push(N);
+ 
+    else {
+ 
+        // Stores the top element
+        int X = S.top();
+ 
+        // Pop the top element
+        S.pop();
+ 
+        // Recurse with remaining elements
+        S = recur(S, N);
+ 
+        // Push the previous
+        // top element again
+        S.push(X);
+    }
+    return S;
+}
+ 
+// Function to insert an element
+// at the bottom of stack
+void insertToBottom(
+    stack<int> S, int N)
+{
+ 
+    // Recursively insert
+    // N at the bottom of S
+    S = recur(S, N);
+ 
+    // Print the stack S
+    while (!S.empty()) {
+        cout << S.top() << " ";
+        S.pop();
+    }
+}
