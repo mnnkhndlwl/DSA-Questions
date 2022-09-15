642d6b59e96876c5db46feb61cf8f34ce2e37470
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-09-15 14:28:36 +0530

    find original array from doubled arraty

---
+++ hash map/Find Original Array From Doubled Array/solution with multiset.cpp
@@ -0,0 +1,29 @@
+// Time Complexity: O(NlogN)
+// Space Complexity: O(N)
+// where N is the number of elements in `changed` 
+class Solution {
+public:
+    // multiset approach
+    vector<int> findOriginalArray(vector<int>& changed) {
+        // if the length of the input is odd, then return {}
+        // because doubled array must have even length
+        if (changed.size() & 1) return {};
+        vector<int> ans;
+        // put all the elements to a multiset
+        multiset<int> s(changed.begin(), changed.end());
+        // keep doing the following logic when there is an element in the multiset
+        while (s.size()) {
+            // get the smallest element
+            int smallest = *s.begin();
+            ans.push_back(smallest);
+            // remove the smallest element in multiset
+            s.erase(s.begin());
+            // if the doubled value of smallest doesn't exist in the multiset
+            // then return {}
+            if (s.find(smallest * 2) == s.end()) return {};
+            // otherwise we can remove its doubled element
+            else s.erase(s.find(smallest * 2));   
+        }
+        return ans;
+    }
+};
