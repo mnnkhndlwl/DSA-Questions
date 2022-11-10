aeebecc7d7eb174bf2530ca73102815d05ecce70
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-11-10 21:36:19 +0530

    remove all adjacent duplicates in a string

---
+++ stl/remove all adjacent duplicates in a string/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string ans;
+        
+        for(int i = 0 ; i < s.size() ; i++)
+        {
+            ans.push_back(s[i]);
+            
+            while(ans.size() && (ans.back()==s[i+1]))
+            {
+                ans.pop_back();
+                i++;
+            }
+        }
+        
+        return ans;
+    }
+};
