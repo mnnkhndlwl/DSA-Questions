e2f70ef750f2dda93c534714a80d6fd042c10931
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-12-12 20:17:49 +0530

    sort list

---
+++ Linked List/sort list/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   ListNode* sortList(ListNode* head) {
+    if(head == NULL) return NULL;
+    else if(head->next == NULL) return head;
+    else{
+        vector<int> v;
+        struct ListNode *result = head;
+        while(result != NULL){
+            v.push_back(result->val);
+            result = result->next;
+        } sort(v.begin(),v.end());
+        result = head;
+        for(int i=0;i<v.size();i++){
+            result->val = v.at(i);
+            result = result->next;
+        } return head;            
+    }
+}
+};
